894dfd6c3ae0adf05ccb44bab156efe0d0e4e744
bahag-HuaY <yun.hua@bahag.com>
2023-11-02 14:58:18 +0100

    renamed Review to Comment, added repo in resource classes

--- routes.py
+++ routes.py
@@ -2,24 +2,32 @@ from flask_restful import Resource
 from repository import Repository
 from flask import request
 
-repo = Repository()
+repository = Repository()
 class Health(Resource):
     def get(self):
         return {'hello': 'from hell'}
     
 
 class EventList(Resource):
+    def __init__(self, repo=repository):
+        self.repo = repo
+
+
     def get(self):
-        return [e.__dict__ for e in repo.events_get_all()]
+        return [e.__dict__ for e in self.repo.events_get_all()]
     
     def post(self):
         data = request.get_json()
-        return repo.event_add(data).__dict__
+        return self.repo.event_add(data).__dict__
 
 
 class Event(Resource):
+    def __init__(self, repo=repository):
+        self.repo = repo
+
+
     def get(self, event_id):
-        event = repo.event_get_by_id(event_id)
+        event = self.repo.event_get_by_id(event_id)
         if event is None:
             return {'error': 'Event not found'}, 404
         return event.__dict__
@@ -27,12 +35,13 @@ class Event(Resource):
 
     def post(self):
         data = request.get_json()
-        return repo.event_add(data).__dict__
+        return self.repo.event_add(data).__dict__
 
 
     def put(self):
         data = request.get_json()
-        return repo.event_update(data).__dict__
+        return self.repo.event_update(data).__dict__
+    
     
     def delete(self, event_id):
-        return repo.event_delete(event_id)
+        return self.repo.event_delete(event_id)
