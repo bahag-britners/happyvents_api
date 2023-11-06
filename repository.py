e047a94759434d75e75bbcdb30141965f16a1b47
bahag-HuaY <yun.hua@bahag.com>
2023-11-06 14:00:38 +0100

    fix: add and update event

--- repository.py
+++ repository.py
@@ -1,4 +1,6 @@
 
+from datetime import date
+from datetime import datetime
 from models import Event, Comment, EventLike, CommentLike, User
 from flask import current_app, g
 
@@ -43,8 +45,8 @@ class Repository():
         if (conn):
             ps_cursor = conn.cursor()
             ps_cursor.execute(
-                "INSERT INTO event(title, description, address, image, event_date, likes, price) VALUES (%s, %s, %s,%s, %s, %s, %s) RETURNING eventid",
-                (data['title'], data['description'], data['address'], '', '', 0, 0))
+                "INSERT INTO events(title, description, address, image, event_date, likes, price) VALUES (%s, %s, %s,%s, %s, %s, %s) RETURNING eventid",
+                (data['title'], data['description'], data['address'], '', datetime.strptime(data['event_date'], "%Y-%m-%d"), 0, data['price']))
             
             conn.commit()
             id = ps_cursor.fetchone()[0]
@@ -57,18 +59,11 @@ class Repository():
         if conn:
             ps_cursor = conn.cursor()
             event_id = data.get('eventid')
-            if event_id is not None:
-                ps_cursor.execute("SELECT eventid FROM events WHERE eventid = %s", (id,))
-                existing_event = ps_cursor.fetchone()
-
-            if existing_event:
-                ps_cursor.execute("UPDATE events SET title= %s, description=%s, address=%s WHERE eventid = %s",
-                                (data.get('title'), data.get('description'), data.get('address'), id))
-                conn.commit()
-                ps_cursor.close()
-                return f"Event with ID {id} updated successfully"
-            else:
-                return f"Event with ID {id} does not exist"
+            ps_cursor.execute("UPDATE events SET title= %s, description=%s, address=%s, event_date=%s, price=%s WHERE eventid = %s",
+                            (data.get('title'), data.get('description'), data.get('address'), datetime.strptime(data.get('event_date'), "%Y-%m-%d"), data.get('price'), event_id))
+            conn.commit()
+            ps_cursor.close()
+            return Event(event_id, data['title'], data['description'], data['address'], data['image'], data['event_date'], data['likes'], data['price'])
 
     def event_delete(self, id):
         conn = self.get_db()
@@ -84,10 +79,3 @@ class Repository():
                 return f"Event with ID {id} deleted successfully"
             else:
                 return f"Event with ID {id} does not exist"
-
-
-    #def reviews_get_by_id(self, id):
-       # return next((r for r in reviews if r.id == id), None)
-    
-    # def review_add(self, data):
-    #     return Comment(data['content'], data['eventId'], len(reviews) + 1)
